8dbc2d6eb33ba2b4e6d5bbd6f10011d796876c1f
bayupw <bayupw@gmail.com>
2022-03-21 11:00:39 +1300

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Aviatrix FireNet Vendor Integration Data Source
+data "aviatrix_firenet_vendor_integration" "this" {
+  vpc_id        = var.transit_firenet_vpc_id
+  instance_id   = var.fw_instance_id
+  firewall_name = var.fw_name
+  vendor_type   = var.fw_instance_vendor_type
+  public_ip     = var.fw_instance_public_ip
+  username      = var.fw_login_username
+  password      = var.fw_login_password
+  save          = true
+}
+
+output "fw_vendor_integration" {
+  value = data.aviatrix_firenet_vendor_integration.this
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "transit_firenet_vpc_id" {
+  type        = string
+  description = "Transit firenet VPC ID"
+}
+
+variable "fw_instance_id" {
+  type        = string
+  description = "Firewall instance id"
+}
+
+variable "fw_name" {
+  type        = string
+  description = "Firewall name"
+  default = null
+}
+
+variable "fw_instance_vendor_type" {
+  type        = string
+  description = "Firewall instance name"
+}
+
+variable "fw_login_username" {
+  type        = string
+  description = "Firewall login username"
+  default     = "admin"
+}
+
+variable "fw_login_password" {
+  type        = string
+  description = "Firewall login username"
+  default     = "Aviatrix123#"
+}
+
+variable "fw_management_ip" {
+  type        = string
+  description = "Firewall management IP address"
+  default = null
+}
+
+variable "fw_route_table" {
+  type        = string
+  description = "Firewall route table"
+  default = null
+}
